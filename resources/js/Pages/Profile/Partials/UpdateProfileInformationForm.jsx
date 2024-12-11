aecdc8fffc398685c4d68d528c97c542eb074041
bellabula <zz225864@gmail.com>
2024-12-11 09:37:11 +0800

    update edit profile

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.jsx
@@ -4,6 +4,7 @@ import PrimaryButton from '@/Components/init/PrimaryButton';
 import TextInput from '@/Components/init/TextInput';
 import { Transition } from '@headlessui/react';
 import { Link, useForm, usePage } from '@inertiajs/react';
+import { useEffect } from 'react';
 
 export default function UpdateProfileInformation({
     mustVerifyEmail,
@@ -16,7 +17,8 @@ export default function UpdateProfileInformation({
         useForm({
             name: user.name,
             email: user.email,
-            phone: user.phone || "",
+            phone: user.phone || "-",
+            birth: user.birth || "-",
         });
     const submit = (e) => {
         e.preventDefault();
@@ -24,8 +26,14 @@ export default function UpdateProfileInformation({
         patch(route('profile.update'));
     };
 
+    useEffect(() => {
+        if (user.birth != "-") {
+            $("#birth").prop('readOnly', true)
+        }
+    })
+
     return (
-        <section className={className}>
+        <section className={className} id='profileEdit'>
             <header>
                 <h2 className="text-lg font-medium text-gray-900">
                     修改基本資料
@@ -37,7 +45,7 @@ export default function UpdateProfileInformation({
             </header>
 
             <form onSubmit={submit} className="mt-5">
-
+                {/* 1. 姓名 */}
                 <div className="mb-3">
                     <InputLabel htmlFor="name" value="姓名*" className='form-label' />
 
@@ -53,7 +61,7 @@ export default function UpdateProfileInformation({
 
                     <InputError className="mt-2 errorMessage" message={errors.name} />
                 </div>
-
+                {/* 2. 信箱 */}
                 <div className="mb-3">
                     <InputLabel htmlFor="email" value="信箱*" className='form-label' />
 
@@ -63,14 +71,14 @@ export default function UpdateProfileInformation({
                         className="form-control-plaintext rounded-pill px-3 editFocus"
                         value={data.email}
                         onChange={(e) => setData('email', e.target.value)}
-                        required
                         autoComplete="username"
+                        required
                         readOnly
                     />
 
                     <InputError className="mt-2" message={errors.email} />
                 </div>
-
+                {/* 3. 信箱驗證 */}
                 {/* {mustVerifyEmail && user.email_verified_at === null && (
                     <div>
                         <p className="mt-2 text-sm text-gray-800">
@@ -93,7 +101,7 @@ export default function UpdateProfileInformation({
                         )}
                     </div>
                 )} */}
-
+                {/* 4. 電話號碼 */}
                 <div className="mb-3">
                     <InputLabel htmlFor="phone" value="電話號碼" className='form-label' />
 
@@ -108,6 +116,7 @@ export default function UpdateProfileInformation({
 
                     <InputError className="mt-2 errorMessage" message={errors.phone} />
                 </div>
+                {/* 5. 生日 */}
                 <div className="mb-3">
                     <InputLabel htmlFor="birth" value="生日" className='form-label' />
 
@@ -119,11 +128,12 @@ export default function UpdateProfileInformation({
                         onChange={(e) => setData('birth', e.target.value)}
                         isFocused
                         autoComplete="birth"
-                        readOnly
                     />
 
                     <InputError className="mt-2 errorMessage" message={errors.birth} />
+                    <span className="small-info">ℹ️ 設定後無法修改</span>
                 </div>
+                {/* 6. 地址 */}
                 <div className="mb-3">
                     <InputLabel htmlFor="address" value="地址" className='form-label' />
 
@@ -138,6 +148,7 @@ export default function UpdateProfileInformation({
 
                     <InputError className="mt-2 errorMessage" message={errors.address} />
                 </div>
+                {/* 7. 付款方式 */}
                 <div className="mb-3">
                     <InputLabel htmlFor="payment" value="付款方式" className='form-label' />
 
@@ -148,26 +159,17 @@ export default function UpdateProfileInformation({
                         onChange={(e) => setData('payment', e.target.value)}
                         isFocused
                         autoComplete="payment"
-                    />
-
-                    <InputError className="mt-2 errorMessage" message={errors.payment} />
-                </div>
-                <div className="mb-3">
-                    <InputLabel htmlFor="invitationCode" value="我的推薦碼" className='form-label' />
-
-                    <TextInput
-                        id="invitationCode"
-                        className="form-control-plaintext rounded-pill px-3 editFocus"
-                        value={data.invitationCode}
-                        onChange={(e) => setData('invitationCode', e.target.value)}
-                        isFocused
-                        autoComplete="invitationCode"
                         readOnly
                     />
 
-                    <InputError className="mt-2 errorMessage" message={errors.invitationCode} />
+                    <InputError className="mt-2 errorMessage" message={errors.payment} />
+                    <div className="edit-button text-end mt-2">
+                        <button type="button" className="btn rounded-pill btn-sm">刪除</button>
+                        &nbsp;
+                        <button type="button" className="btn rounded-pill btn-sm">新增</button>
+                    </div>
                 </div>
-
+                {/* 確認修改資料按鈕 */}
                 <div className="edit-button text-end mt-4">
                     {/* <PrimaryButton disabled={processing}>Save</PrimaryButton> */}
                     <button className="btn rounded-pill">確認修改資料</button>
@@ -180,7 +182,7 @@ export default function UpdateProfileInformation({
                         leaveTo="opacity-0"
                     >
                         <p className="text-sm text-gray-600">
-                            Saved.
+                            資料已修改.
                         </p>
                     </Transition>
                 </div>
