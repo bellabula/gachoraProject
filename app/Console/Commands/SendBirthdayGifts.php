5a70a6c52bf8640452675b6607cbf513b7f2a325
versurasss <cherry20020125@gmail.com>
2024-12-27 14:41:18 +0800

    1227

--- app/Console/Commands/SendBirthdayGifts.php
+++ app/Console/Commands/SendBirthdayGifts.php
@@ -8,23 +8,31 @@ use App\Http\Controllers\BirthdayController;
 class SendBirthdayGifts extends Command
 {
     /**
-     * 命令的名稱及簽名。
+     * 命令名稱
+     *
+     * @var string
      */
+    // 給 Artisan 命令的標識，用於執行這個任務。
     protected $signature = 'birthday:send-gifts';
 
     /**
-     * 命令的描述。
+     * 命令描述
+     *
+     * @var string
      */
-    protected $description = 'Send birthday gifts to users';
+    // 如果我打php artisan list，會出現的文字描述 (讓開發者知道這個命令的用途)
+    protected $description = '發送生日禮金和祝福郵件';
 
     /**
-     * 執行命令。
+     * 執行命令
+     *
+     * @return int
      */
+    // 命令的執行入口點
     public function handle()
     {
-        // 呼叫控制器方法
-        (new BirthdayController())->sendBirthdayGifts();
-        
-        $this->info('Birthday gifts sent successfully!');
+        // 調用某控制器的某個function
+        app(BirthdayController::class)->sendBirthdayGifts();
+        return Command::SUCCESS;
     }
 }
